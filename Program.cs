109d713c6841da1ac07f548978b4f022a721afa6
Jim Vacca <jamesvac@microsoft.com>
2022-11-29 16:00:18 -0500

    Fusion v1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using xCel = Microsoft.Office.Interop.Excel;
 
 namespace ConsoleExcel2
 {
-    internal class Program
+     class Program
     {
         static void Main(string[] args)
         {
@@ -14,20 +14,21 @@ namespace ConsoleExcel2
             xCel.Application myExcel = new();
             xCel.Workbook myWorkbook;
             xCel.Worksheet myWorkssheet;
+            
+            
 
             myWorkbook = myExcel.Workbooks.Open(OneAskFile);
             myWorkssheet = myWorkbook.Worksheets[1];
             int lastRow = myWorkssheet.Cells.SpecialCells(xCel.XlCellType.xlCellTypeLastCell).Row;
            
             //int row = 2;
-            int col = 4;            
-
+            int col = 4;
 
             for (int row = 2; row <= lastRow; row++)
             {
                 if (myWorkssheet.Cells[row, col + 1].Value2 != null)
 
-                    myWorkssheet.Cells[row, col] = OneAskClassification(myWorkssheet.Cells[row, col + 1].Value2);
+                    myWorkssheet.Cells[row, col] = ClassifyOneAsk(myWorkssheet.Cells[row, col + 1].Value2);
 
                 else
 
@@ -41,19 +42,72 @@ namespace ConsoleExcel2
             Console.WriteLine("Row count: "+lastRow);
             Console.WriteLine("End!");
 
-
         } // end main
- 
-
        
-        private static string OneAskClassification(string Title)
+        private static string ClassifyOneAsk(string Title)
         {
-            string oneaskClass = "classification not set";            
-            
-            return oneaskClass;
+            OneAskClassification = "Classification started";            
+
+            if (ClassifyFusion(Title))
+                OneAskClassification = "Power";
+                
+            //if (ClassifyJava())
+            //        OneAskClassification = "Java";
+            //        else if (ClassifyIntegration())
+            //            OneAskClassification = "Power";
+            //            if (ClassifyCloudNative())
+            //                OneAskClassification = "Power";
+            //                if (ClassifyMisc())
+            //                   OneAskClassification = "Classification not set";
+            //                   else
+            //                        OneAskClassification = "Classification not set";
+
+            return OneAskClassification;
 
         } // end OneAskClassification
-    
+
+
+        private static bool ClassifyFusion(string Title)
+        {
+
+            List<string> Fusion_Terms = new List<string>()
+                { "Power", "Fusion", "RPA", "LC/NC", "Low Code"  };
+
+            foreach (string Fusion_Term in Fusion_Terms)
+            {
+                if (Title.Contains(Fusion_Term, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    OneAskClassification = "LC/NC";
+                    break;
+                }
+            }
+            
+
+            return false;
+        }
+
+        private static bool ClassifyMisc()
+        {
+            return true;
+        }
+        private static bool ClassifyCloudNative()
+        {
+            return true;
+        }
+        private static bool ClassifyIntegration()
+        {
+            return true;
+        }
+
+        private static bool ClassifyJava()
+        {
+            return true;
+        }
+
+        
+
+        static string OneAskClassification = "classification created";
+
     } // end class Program
 
  } // end namespace
