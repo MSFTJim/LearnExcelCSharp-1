18d7c892e2b3792f233f19a9b0f2b2edc8aed365
Jim Vacca <jamesvac@microsoft.com>
2022-11-30 18:42:48 -0500

    java and int

--- Program.cs
+++ Program.cs
@@ -47,19 +47,16 @@ namespace ConsoleExcel2
        
         private static string ClassifyOneAsk(string Title)
         {
-            OneAskClassification = "Classification started";            
+            OneAskClassification = "Classification started";
 
-            if (ClassifyFusion(Title))
-                OneAskClassification = "Power";
-
-            if (ClassifyJava())
-                OneAskClassification = "Java";
-            else if (ClassifyIntegration())
-                OneAskClassification = "Power";
-            if (ClassifyCloudNative(Title))
-                OneAskClassification = "Power";
-            if (ClassifyMisc())
-                OneAskClassification = "Misc";
+            if (!ClassifyFusion(Title))
+                if (!ClassifyCloudNative(Title))
+                    if (!ClassifyJava(Title))
+                        if (!ClassifyIntegration(Title))                    
+                            { OneAskClassification = "Classification not set"; };            
+            
+            //if (ClassifyMisc())
+            //    OneAskClassification = "Misc";
             //else
             //    OneAskClassification = "Classification not set";
 
@@ -67,10 +64,91 @@ namespace ConsoleExcel2
 
         } // end OneAskClassification
 
+        private static bool ClassifyIntegration(string Title)
+        {
+            bool ClassifiedAsIntegration = false;
+            int IntegrationCount = 0;
+
+            List<string> APIM_Terms = new List<string>()
+                { "APIM", "API Management" };
+            List<string> SB_Terms = new List<string>()
+                { "Service Bus", "ServiceBus"};
+            List<string> LA_Terms = new List<string>()
+                { "Logic Apps","LogicApps"};
+
+            List<string> Integration_Terms = new List<string>()
+                { "event", "Functions"};
+            Integration_Terms.AddRange(APIM_Terms);
+            Integration_Terms.AddRange(LA_Terms);
+            Integration_Terms.AddRange(SB_Terms);
+
+            foreach (string IntegrationTerm in Integration_Terms)
+            {
+                if (Title.Contains(IntegrationTerm, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    ClassifiedAsIntegration = true;
+                    OneAskClassification = IntegrationTerm;
+                    IntegrationCount++;
+                    if (IntegrationCount > 1)
+                    {
+                        OneAskClassification = "Integration";
+                        break;
+                    }
+
+                    if (APIM_Terms.Any(s => s.Equals(IntegrationTerm, StringComparison.CurrentCultureIgnoreCase)))
+                        OneAskClassification = "APIM";
+                    else
+                    if (SB_Terms.Any(s => s.Equals(IntegrationTerm, StringComparison.CurrentCultureIgnoreCase)))
+                        OneAskClassification = "Service Bus";
+                    else
+                    if (LA_Terms.Any(s => s.Equals(IntegrationTerm, StringComparison.CurrentCultureIgnoreCase)))
+                        OneAskClassification = "Logic Apps";
+                    else
+                    if (Title.Contains("Event", StringComparison.CurrentCultureIgnoreCase) && Title.Contains("Hub", StringComparison.CurrentCultureIgnoreCase))
+                        OneAskClassification = "Event Hubs";
+                    else
+                    if (Title.Contains("Event", StringComparison.CurrentCultureIgnoreCase) && Title.Contains("Grid", StringComparison.CurrentCultureIgnoreCase))
+                        OneAskClassification = "Event Grid";
+                    else
+                    if (Title.Contains("Event", StringComparison.CurrentCultureIgnoreCase) && Title.Contains("Driven", StringComparison.CurrentCultureIgnoreCase))
+                        OneAskClassification = "Event Driven Arch";
+                    
+
+                } // end if contains
+
+            } // end foreach
+
+
+
+            return ClassifiedAsIntegration;
+        }
+
+        private static bool ClassifyJava(string Title)
+        {
+            bool ClassifiedAsJava = false;
+
+            if (Title.Contains("spring", StringComparison.CurrentCultureIgnoreCase))
+            { OneAskClassification = "Spring";
+              ClassifiedAsJava = true;
+            }
+            else
+                if (Title.Contains("java", StringComparison.CurrentCultureIgnoreCase))
+            {
+                OneAskClassification = "Java";
+                ClassifiedAsJava = true;
+            }          
+
+
+            return ClassifiedAsJava;
+
+        }
+
 
         private static bool ClassifyFusion(string Title)
         {
 
+            bool ClassifiedAsFusion = false;
+            
             List<string> Fusion_Terms = new List<string>()
                 { "Power", "Fusion", "RPA", "LC/NC", "Low Code"  };
 
@@ -79,12 +157,13 @@ namespace ConsoleExcel2
                 if (Title.Contains(Fusion_Term, StringComparison.CurrentCultureIgnoreCase))
                 {
                     OneAskClassification = "LC/NC";
-                    return true;
+                    ClassifiedAsFusion = true;
+                    break;
                 }
             }
             
 
-            return false;
+            return ClassifiedAsFusion;
         }
 
         private static bool ClassifyMisc()
@@ -93,71 +172,88 @@ namespace ConsoleExcel2
         }
         private static bool ClassifyCloudNative(string Title)
         {
-            int CloudNativeCount = 0; 
-            List<string> CN_SingleTerms = new List<string>()
-                { "AKS","ARO","ACA", "container","k8s", "kubernetes"};
-            //  Container App covered by container
+            bool ClassifiedAsCN = false;
+
+            int CloudNativeCount = 0;
+            int AROCount = 0;
+
 
             List<string> CN_AKSTerms = new List<string>()
                 { "AKS","kubernetes","Kubernetes","k8s"};
+            List<string> CN_AROTerms = new List<string>()
+                { "ARO","redhat","red hat","openshift","open shift"};
             List<string> CN_ACATerms = new List<string>()
-                { "ACA","container app","ContainerApp"};           
-               
-                foreach (string CNTerm in CN_SingleTerms)
+                { "ACA","container app","ContainerApp"};
+
+            List<string> CN_Terms = new List<string>()
+                { "container"};
+
+            CN_Terms.AddRange(CN_AKSTerms);
+            CN_Terms.AddRange(CN_AROTerms);
+            CN_Terms.AddRange(CN_ACATerms);
+
+
+            //  Container App covered by container
+
+            foreach (string CNTerm in CN_Terms)
                 {
                     if (Title.Contains(CNTerm, StringComparison.CurrentCultureIgnoreCase))
                     {
                         // you made it this far so count it as a CN terms and capture which ever term we found
+                        ClassifiedAsCN = true;
                         CloudNativeCount++;
                         OneAskClassification = CNTerm;
 
-                        // if count is greater than 1, we call it CN and are done
-                        if (CloudNativeCount > 1)
-                        {
-                            OneAskClassification = "Cloud Native";
-                            return false;
-                        }
-                            
+                    // Tagging processing
 
-                        // Tagging processing
-                        
-                        if (Title.Contains("container", StringComparison.CurrentCultureIgnoreCase))
-                            if (Title.Contains("container app", StringComparison.CurrentCultureIgnoreCase))
-                                OneAskClassification = "ACA";
-                            else
-                                if (Title.Contains("ContainerApp", StringComparison.CurrentCultureIgnoreCase))
-                                OneAskClassification = "ACA";
-                            else
-                                OneAskClassification = "Cloud Native";
-
-                        if (CN_ACATerms.Any(s => s.Equals(CNTerm, StringComparison.CurrentCultureIgnoreCase)))
+                    if (Title.Contains("container", StringComparison.CurrentCultureIgnoreCase))                    
+                        if (Title.Contains("container app", StringComparison.CurrentCultureIgnoreCase))
                             OneAskClassification = "ACA";
-
-                        if (CN_AKSTerms.Any(s => s.Equals(CNTerm, StringComparison.CurrentCultureIgnoreCase)))
-                            OneAskClassification = "AKS";
-
-                        string repattern = @"(cloud).(native)";
-                        if (Regex.IsMatch(CNTerm, repattern, RegexOptions.IgnoreCase))
+                        else
+                            if (Title.Contains("ContainerApp", StringComparison.CurrentCultureIgnoreCase))
+                            OneAskClassification = "ACA";
+                        else
                             OneAskClassification = "Cloud Native";
+                    
 
-                    }  // if term contains
+                    if (CN_ACATerms.Any(s => s.Equals(CNTerm, StringComparison.CurrentCultureIgnoreCase)))                    
+                        OneAskClassification = "ACA";
+                    
 
-                } // foreach terms
-             
+                    if (CN_AKSTerms.Any(s => s.Equals(CNTerm, StringComparison.CurrentCultureIgnoreCase)))                    
+                        OneAskClassification = "AKS";
+
+
+                    if (CN_AROTerms.Any(s => s.Equals(CNTerm, StringComparison.CurrentCultureIgnoreCase)))
+                    {
+                        AROCount++;
+                        if (AROCount > 1)
+                            CloudNativeCount--;
+                        OneAskClassification = "ARO";
+                    }
+                    string repattern = @"(cloud).(native)";
+                    if (Regex.IsMatch(CNTerm, repattern, RegexOptions.IgnoreCase))
+                        OneAskClassification = "Cloud Native";
+
+                    // if count is greater than 1, we call it CN and are done
+                    if (CloudNativeCount > 1)
+                    {
+                        OneAskClassification = "Cloud Native";
+                        break;
+                    }
+
+                }  // if term contains
+
+                } // foreach terms             
 
                 //Console.WriteLine("final value: " + OneAskClassification + ". Count: " + CloudNativeCount + ", " + Title);          
 
-            return false;
-        } // end cn tagging
-        private static bool ClassifyIntegration()
-        {
-            return false;
-        }
+                return ClassifiedAsCN;
 
-        private static bool ClassifyJava()
-        {
-            return false;
-        }
+        } // end cn tagging
+      
+
+      
 
         
 
